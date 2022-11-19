30c27f77919120bc7bbd234305c5f582c301cc73
Tongchen He <tongchenhe2002@gmail.com>
2022-11-19 12:48:13 -0800

    sql updated

--- SQL Statements/Key_Hook_1_create.sql
+++ SQL Statements/Key_Hook_1_create.sql
@@ -96,96 +96,96 @@ CREATE TABLE rooms (
 -- Reference: Table_9_hooks_fk_01 (table: hook_door_opening)
 ALTER TABLE hook_door_opening ADD CONSTRAINT Table_9_hooks_fk_01
     FOREIGN KEY (hook_number)
-    REFERENCES hooks (hook_number)
-    NOT DEFERRABLE
+    REFERENCES hooks (hook_number)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: doors_door_names_fk_01 (table: doors)
 ALTER TABLE doors ADD CONSTRAINT doors_door_names_fk_01
     FOREIGN KEY (door_name)
-    REFERENCES door_names (name)
-    NOT DEFERRABLE
+    REFERENCES door_names (name)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: doors_rooms_fk_01 (table: doors)
 ALTER TABLE doors ADD CONSTRAINT doors_rooms_fk_01
     FOREIGN KEY (room_number, building_name)
-    REFERENCES rooms (room_number, building_name)
-    NOT DEFERRABLE
+    REFERENCES rooms (room_number, building_name)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: hook_door_opening_doors_fk_01 (table: hook_door_opening)
 ALTER TABLE hook_door_opening ADD CONSTRAINT hook_door_opening_doors_fk_01
     FOREIGN KEY (room_number, building_name, door_name)
-    REFERENCES doors (room_number, building_name, door_name)
-    NOT DEFERRABLE
+    REFERENCES doors (room_number, building_name, door_name)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: key_issue_loss_key_issue_fk_01 (table: key_issue_loss)
 ALTER TABLE key_issue_loss ADD CONSTRAINT key_issue_loss_key_issue_fk_01
     FOREIGN KEY (issue_number)
-    REFERENCES key_issue (issue_number)
-    NOT DEFERRABLE
+    REFERENCES key_issue (issue_number)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: key_issue_return_key_issue_fk_01 (table: key_issue_return)
 ALTER TABLE key_issue_return ADD CONSTRAINT key_issue_return_key_issue_fk_01
     FOREIGN KEY (issue_number)
-    REFERENCES key_issue (issue_number)
-    NOT DEFERRABLE
+    REFERENCES key_issue (issue_number)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: key_issue_room_requests_fk_01 (table: key_issue)
 ALTER TABLE key_issue ADD CONSTRAINT key_issue_room_requests_fk_01
     FOREIGN KEY (request_id)
-    REFERENCES room_requests (request_id)
-    NOT DEFERRABLE
+    REFERENCES room_requests (request_id)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: key_requests_keys_fk_01 (table: key_issue)
 ALTER TABLE key_issue ADD CONSTRAINT key_requests_keys_fk_01
     FOREIGN KEY (hook_number, key_number)
-    REFERENCES keys (hook_number, key_number)
-    NOT DEFERRABLE
+    REFERENCES keys (hook_number, key_number)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: keys_hooks (table: keys)
 ALTER TABLE keys ADD CONSTRAINT keys_hooks
     FOREIGN KEY (hook_number)
-    REFERENCES hooks (hook_number)
-    NOT DEFERRABLE
+    REFERENCES hooks (hook_number)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: room_requests_employees_fk_01 (table: room_requests)
 ALTER TABLE room_requests ADD CONSTRAINT room_requests_employees_fk_01
     FOREIGN KEY (employee_id)
-    REFERENCES employees (id)
-    NOT DEFERRABLE
+    REFERENCES employees (id)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: room_requests_rooms_fk_01 (table: room_requests)
 ALTER TABLE room_requests ADD CONSTRAINT room_requests_rooms_fk_01
     FOREIGN KEY (room_number, building_name)
-    REFERENCES rooms (room_number, building_name)
-    NOT DEFERRABLE
+    REFERENCES rooms (room_number, building_name)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
 -- Reference: rooms_buildings_fk_01 (table: rooms)
 ALTER TABLE rooms ADD CONSTRAINT rooms_buildings_fk_01
     FOREIGN KEY (building_name)
-    REFERENCES buildings (name)
-    NOT DEFERRABLE
+    REFERENCES buildings (name)  
+    NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
